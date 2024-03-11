f7c98bb7d5521dac377dcf6ff8becdc7158a4d67
YUE QIU <qiuyue@outlook.jp>
2024-03-11 16:10:51 +0900

    Delete STVchrono_dataset directory

--- STVchrono_dataset/dataset.sh
+++
@@ -1,4 +0,0 @@
-## Annotation file for the continual change captioning (image pair) task
-stvchrono_v1_continual-change-captioning_image-pair.json
-
-## Annotation file for the continual change captioning (image pair) task
