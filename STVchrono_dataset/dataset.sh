b0402786cad5424c4e6c0e492246b14bf56f5ca5
YUE QIU <qiuyue@outlook.jp>
2024-03-11 16:10:10 +0900

    Update dataset.sh

--- STVchrono_dataset/dataset.sh
+++ STVchrono_dataset/dataset.sh
@@ -1,3 +1,4 @@
 ## Annotation file for the continual change captioning (image pair) task
+stvchrono_v1_continual-change-captioning_image-pair.json
 
 ## Annotation file for the continual change captioning (image pair) task
